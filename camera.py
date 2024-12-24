7929bda518d4a2f54fdf802742600595ea008b7c
Tuominiemi William <william.tuominiemi@aalto.ficd>
2024-12-24 16:30:29 +0200

    only detect eyes within a face

--- camera.py
+++ camera.py
@@ -8,6 +8,7 @@ frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (frame_width, frame_height))
 
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 while True:
@@ -17,11 +18,18 @@ while True:
 
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    eyes = eye_cascade.detectMultiScale(gray_frame, scaleFactor=1.1, minNeighbors=5)
-    for (x, y, w, h) in eyes:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    faces = face_cascade.detectMultiScale(gray_frame, 1.1, 4)
 
-    blurred_frame = cv2.GaussianBlur(frame, (15, 15), 0)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 255), 2)
+
+        face_roi = gray_frame[y:y+h, x:x+w]
+        
+        eyes = eye_cascade.detectMultiScale(face_roi)
+
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(frame, (x+ex, y+ey), (x+ex+ew, y+ey+eh), (0, 0, 255), 2)
+
 
     out.write(frame) 
 
