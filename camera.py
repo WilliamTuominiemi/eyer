47e969ebb3640f5ae2ecbed96bbd38d9d6384664
Tuominiemi William <william.tuominiemi@aalto.ficd>
2024-12-24 21:05:21 +0200

    calculate where user is vieweing on screen

--- camera.py
+++ camera.py
@@ -19,9 +19,21 @@ def detect_eyes(img_gray, frame_height):
     for (x, y, w, h) in coords:
         if y + h <= frame_height / 2:
             new_y = int(y + (h * 0.25))
-            new_h = int(h * 0.75)
+            new_h = int(h * 0.5)
             valid_coords.append((x, new_y, w, new_h))
     return valid_coords
+
+def calculate_view_point(eye_rect, pupil_pos):
+    ex, ey, ew, eh = eye_rect
+    px, py = pupil_pos
+
+    x_ratio = px - (ew / 2)
+    y_ratio = py - (eh / 2)
+
+    screen_x = int((frame_width / 2)+x_ratio *100)
+    screen_y = int((frame_height / 2)+y_ratio *100)
+    
+    return (screen_x, screen_y)
 
 while True:
     ret, frame = cam.read()
@@ -33,6 +45,7 @@ while True:
     faces = face_cascade.detectMultiScale(gray_frame, 1.3, 5)
 
     threshold = 25
+    gaze_point=(0,0)
 
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
@@ -43,27 +56,34 @@ while True:
         eyes = detect_eyes(face_roi, h)
 
         for (ex, ey, ew, eh) in eyes:
-           eye_roi = cv2.equalizeHist(face_roi[ey:ey+eh, ex:ex+ew])
-           _, eye_roi_bw = cv2.threshold(eye_roi, threshold, 255, cv2.THRESH_BINARY)
+            eye_roi = cv2.equalizeHist(face_roi[ey:ey+eh, ex:ex+ew])
+            _, eye_roi_bw = cv2.threshold(eye_roi, threshold, 255, cv2.THRESH_BINARY)
            
-           cv2.rectangle(frame, (x+ex, y+ey), (x+ex+ew, y+ey+eh), (0, 0, 255), 1)
+            cv2.rectangle(frame, (x+ex, y+ey), (x+ex+ew, y+ey+eh), (0, 0, 255), 1)
 
-           eye_roi_bw = cv2.erode(eye_roi_bw, None, iterations=2)
-           eye_roi_bw = cv2.dilate(eye_roi_bw, None, iterations=4)
-           eye_roi_bw = cv2.medianBlur(eye_roi_bw, 5)
+            eye_roi_bw = cv2.erode(eye_roi_bw, None, iterations=2)
+            eye_roi_bw = cv2.dilate(eye_roi_bw, None, iterations=4)
+            eye_roi_bw = cv2.medianBlur(eye_roi_bw, 5)
            
-           detector = cv2.SimpleBlobDetector_create()
-           keypoints = detector.detect(eye_roi_bw)
+            detector = cv2.SimpleBlobDetector_create()
+            keypoints = detector.detect(eye_roi_bw)
            
-           for keypoint in keypoints:
-               center = (int(keypoint.pt[0]), int(keypoint.pt[1]))
-               radius = int(keypoint.size / 2)
-               cv2.circle(frame[y+ey:y+ey+eh, x+ex:x+ex+ew], center, radius, (0, 0, 255), 2)
+            for keypoint in keypoints:
+                center = (int(keypoint.pt[0]), int(keypoint.pt[1]))
+
+                gaze_point = calculate_view_point((ex, ey, ew, eh), center)
+
+                radius = int(keypoint.size / 2)
+                cv2.circle(frame[y+ey:y+ey+eh, x+ex:x+ex+ew], center, radius, (0, 0, 255), 2)
    
+    
+    cv2.circle(frame, gaze_point, 10, (255, 0, 0), -1)
 
     out.write(frame) 
 
-    cv2.imshow('Camera', frame)
+    flipped_frame = cv2.flip(frame, 1)
+
+    cv2.imshow('Camera', flipped_frame)
 
     if cv2.waitKey(1) == ord('q'):
         break
